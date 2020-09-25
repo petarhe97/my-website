a076c714d25fa1281f921ac4a335ae3c73ea1cd6
Peter He <peter.he97@gmail.com>
2020-09-25 10:44:01 -0400

    Added footer

--- src/components/main-container.js
+++ src/components/main-container.js
@@ -33,8 +33,16 @@ class MainContainer extends Component {
                              <div>
                                    {displayedView}
                              </div>
-                        </main>  
+                        </main>
                     </div>
+                    <footer id="footer" class="container-fluid">
+                        <ul class="list-inline col-sm-12" style={{"margin-top": "10px", "margin-bottom": "10px"}}>
+                            <li style={{display: "inline-block", padding: "10px"}}><a href="https://github.com/petarhe97" target="_blank" id="image-button"><img src="/GitHub.png"/></a></li>
+                            <li style={{display: "inline-block", padding: "10px"}}><a href="https://www.linkedin.com/in/peter-he-549933117/" target="_blank" id="image-button"><img src="/linkedin.png"/></a></li>
+                        </ul>
+                        <div class="text-center">&copy; {(new Date().getFullYear())}, Zi Hao (Peter) He. All rights reserved.</div>
+                        <div class="text-center">Hosted by Github Pages</div> 
+                    </footer>   
             </div>
         )
     }
